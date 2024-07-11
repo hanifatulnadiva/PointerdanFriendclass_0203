3cfa2cd242afe51a3ab46cc72a12c39309925c16
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-07-11 13:17:47 +0700

    membuat int main

--- contoh1.cpp
+++ contoh1.cpp
@@ -25,3 +25,19 @@ class segitiga
             return p.panjang;
        }
 };
+int main() {
+    persegiPanjang pp;
+    segitiga sg;
+
+    cout << "Masukkan panjang: ";
+    cin >> pp.panjang;
+    cout << "Masukkan lebar: ";
+    cin >> pp.lebar;
+    cout << "Masukkan tinggi: ";
+    cin >> sg.tinggi;
+    cout << "Masukkan alas: ";
+    cin >> sg.alas;
+
+    cout << "Luas Persegi Panjang: " << pp.luasPersegiPanjang(pp.panjang, pp.lebar) << endl;
+    cout << "Luas Segitiga: " << sg.luassegitiga(sg.alas, sg.tinggi) << endl;
+}
