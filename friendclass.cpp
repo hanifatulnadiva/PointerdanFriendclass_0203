6364c9dcfae4bca3aee820f5984128d70e6ce95b
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-07-11 13:38:11 +0700

    menambahkan class siswa beserta member dan akses modifiernya

--- friendclass.cpp
+++ friendclass.cpp
@@ -9,3 +9,11 @@ class orang {
     void setNama(string pNama);
     friend class siswa;
 };
+class siswa {
+    private :
+        int id;
+
+    public :
+        void setId(int pId);
+        void displayAll(orang &a);
+};
